ea00f63146993b8b2d956f20eddb533f47376a0f
ESMON13 <125477208+ESMON13@users.noreply.github.com>
2023-12-21 12:35:30 +0100

    0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile alx-system_engineering-devops/0x0B-ssh/school',
+}
